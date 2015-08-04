73256eade26c36c11c3b4af1925701486ae182d1
baziii <bqfche@126.com>
2015-08-04 13:02:12 +0800

    add login softinput hiden after login

--- TripApp/src/main/java/com/cuitrip/login/LoginActivity.java
+++ TripApp/src/main/java/com/cuitrip/login/LoginActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -224,6 +225,13 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener,
                     }
 
                     LoginInstance.update(LoginActivity.this, info);
+
+                    try {
+                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                     finish();
                 }
             }
