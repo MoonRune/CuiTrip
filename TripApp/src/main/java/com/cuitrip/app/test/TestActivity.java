4188be9aba2f41a389d070bb13838cfb79d01010
baziii <bqfche@126.com>
2015-08-27 11:59:05 +0800

    ok

---
+++ TripApp/src/main/java/com/cuitrip/app/test/TestActivity.java
@@ -0,0 +1,21 @@
+package com.cuitrip.app.test;
+
+import android.os.Bundle;
+
+import com.cuitrip.service.R;
+import com.lab.app.BaseActivity;
+
+/**
+ * Created by baziii on 15/8/27.
+ */
+public class TestActivity extends BaseActivity {
+    String text="嘉义旧名诸罗县，有着世界著名的阿里山日出，看到是真的会感动的。除此之外，大家对嘉义的认识还有多少?除了阿里山嘉义还有很多温馨贴心的小地方，就由我这个小护士带你重新认识嘉义市吧!\\n桧意森活村早期为日籍基层职员的宿舍群，在越过一甲子以上的岁月洗礼之后，以日式传统工法以及尽可能的原材料进行修复，目前保留下来28栋木构造历史建筑，在这里你可以伴随着薪薪的桧木香逛著文创小店。\\n<div>< img src=\\\"http://cuitrip.oss-cn-shenzhen.aliyuncs.com/224_1438681829038\\\" width=\\\"100\\\" /></div><div>< img src=\\\"http://cuitrip.oss-cn-shenzhen.aliyuncs.com/224_1438681930879\\\" width=\\\"100\\\" /></div>被称之为北门驿的历史景点，起源于日治时期。日本人为开采森林资源、进行伐木计划而兴建阿里山森林铁路，并将运材的终始站设在嘉义北门，称之为北门驿。\\n<div>< img src=\\\"http://cuitrip.oss-cn-shenzhen.aliyuncs.com/224_1438681857427\\\" width=\\\"100\\\" /></div><div>< img src=\\\"http://cuitrip.oss-cn-shenzhen.aliyuncs.com/224_1438681851053\\\" width=\\\"100\\\" /></div>之后我们会去往铁道艺术村——旅行的途中，就算似懂非懂的走马看花，也能得到不同的旅行乐趣，静谧的旧铁道，少了轰隆作响的火车运行声，也为此行添加了些许的艺术氛围。\\n<div>< img src=\\\"http://cuitrip.oss-cn-shenzhen.aliyuncs.com/224_1438681957510\\\" width=\\\"100\\\" /></div><div>< img src=\\\"http://cuitrip.oss-cn-shenzhen.aliyuncs.com/224_1438681948868\\\" width=\\\"100\\\" /></div>通常要了解一个城市，可以从他的博物馆开始。嘉义博物馆的展示品包罗万象，从化石到现代，也随着历史的演进做了详细的纪录，古往今来，视古明";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_test_muti);
+//        ((MutiEditView) findViewById(R.id.muti)).setText(text);
+    }
+
+}
---
+++ TripApp/src/main/java/com/lab/widget/MutiEditView.java
@@ -0,0 +1,208 @@
+package com.lab.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.method.KeyListener;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.cuitrip.app.MainApplication;
+import com.cuitrip.service.R;
+import com.lab.utils.ImageHelper;
+import com.lab.utils.LogHelper;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * Created by baziii on 15/8/27.
+ */
+public class MutiEditView extends LinearLayout {
+
+    public MutiEditView(Context context) {
+        super(context);
+        init();
+    }
+
+    public MutiEditView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MutiEditView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(21)
+    public MutiEditView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    public void init() {
+        setOrientation(VERTICAL);
+    }
+
+    public void buildEdit() {
+        if (getChildCount() == 0) {
+            EditText editText=buildEdittext();
+            editText.setHint(" please input");
+        }
+    }
+
+    public void insertImage(String url){
+        buildImage(url);
+        appendEdit().requestFocus();
+    }
+
+    public void setText(String introduce) {
+        while (!TextUtils.isEmpty(introduce) && introduce.indexOf("<div>") >= 0) {
+            if (introduce.indexOf("<div>") >= 0) {
+                String temp = introduce.substring(0, introduce.indexOf("<div>"));
+                introduce = introduce.substring(introduce.indexOf("<div>") + 5);
+                LogHelper.e("now ", introduce);
+                buildTextView(temp);
+                temp = introduce.substring(0, introduce.indexOf("</div>"));
+                buildImage(temp);
+                introduce = introduce.substring(introduce.indexOf("</div>") + 6);
+            }
+        }
+        buildTextView(introduce);
+    }
+
+    @Override
+    public void addView(View child) {
+        for (int i =  getChildCount(); i>0 ; i--) {
+            if (
+                    ( getChildAt(i) instanceof TextView )&&
+                            TextUtils.isEmpty(((TextView) getChildAt(i)).getText().toString())){
+                removeViewAt(i);
+            }
+        }
+        super.addView(child);
+    }
+
+    public EditText buildEdittext() {
+        EditText textView = (EditText) LayoutInflater.from(getContext()).inflate(R.layout.edit, this, false);
+        addView(textView);
+        return textView;
+    }
+
+    public EditText appendEdit(){
+        EditText editText = buildEdittext();
+        editText.setKeyListener(new KeyListener() {
+            @Override
+            public int getInputType() {
+                return 0;
+            }
+
+            @Override
+            public boolean onKeyDown(View view, Editable text, int keyCode, KeyEvent event) {
+                if (KeyEvent.KEYCODE_DEL == event.getKeyCode()) {
+                    int preIndex = findPreCildIndex(view);
+                        if (preIndex<=0){
+                            removeViewAt(preIndex);
+                            return true;
+                        }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onKeyUp(View view, Editable text, int keyCode, KeyEvent event) {
+                return false;
+            }
+
+            @Override
+            public boolean onKeyOther(View view, Editable text, KeyEvent event) {
+                return false;
+            }
+
+            @Override
+            public void clearMetaKeyState(View view, Editable content, int states) {
+
+            }
+        });
+        return  editText;
+    }
+
+    public int findPreCildIndex(View view){
+        for (int i = 0; i < getChildCount(); i++) {
+           if( getChildAt(i).equals(view)){
+               return i;
+           }
+        }
+        return -1;
+    }
+
+    public void buildTextView(String temp) {
+        if (!TextUtils.isEmpty(temp)) {
+            TextView textView = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.tv, this, false);
+            textView.setText(temp);
+
+            addView(textView);
+        }
+    }
+
+    public void buildImage(String image) {
+
+        try {
+            image = image.substring(image.indexOf("http:"), image.indexOf("\"", image.indexOf("http:")));
+            ImageView textView = (ImageView) LayoutInflater.from(getContext()).inflate(R.layout.tv_image, this, false);
+            addView(textView);
+//            ImageHelper.displayCtImage(image, textView, null);
+            startDiaplay(textView, image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startDiaplay(final ImageView imageView, final String url) {
+        imageView.setImageResource(R.drawable.ct_default);
+        new AsyncTask() {
+            Bitmap bitmap;
+            float height;
+            float width;
+
+            @Override
+            protected Object doInBackground(Object[] params) {
+                bitmap = ImageLoader.getInstance().loadImageSync(url, ImageHelper.getDefaultDisplayImageOptions());
+                int tempWidth = bitmap.getWidth();
+                height = bitmap.getHeight();
+                width = MainApplication.getInstance().getPageWidth();
+                height = width * height / tempWidth;
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                try {
+                    if (bitmap != null && imageView != null) {
+                        imageView.setImageBitmap(bitmap);
+                        ViewGroup.LayoutParams params = imageView.getLayoutParams();
+                        params.width = (int) width;
+                        params.height = (int) height;
+                        imageView.setLayoutParams(params);
+                        imageView.invalidate();
+                        LogHelper.e("omg", "set image " + width + "|" + height);
+                    }
+                } catch (Exception e) {
+                }
+                super.onPostExecute(o);
+            }
+
+
+        }.execute(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+}
